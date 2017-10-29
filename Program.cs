2e5643dae3f0a8616f1ee8b9e61ab33612cf55e6
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-10-29 07:58:04 -0400

    Improved the test logic

--- Program.cs
+++ Program.cs
@@ -10,10 +10,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NuclearPizza {
+    /// <summary>
+    /// Set up a rudimentary test for the NuclearPizza class
+    /// </summary>
     class Program {
         static void Main(string[] args) {
             NuclearPizza myPizza = new NuclearPizza();
             myPizza.Kw = 1000;                      // It's a 1000 kilowatt pizza. That seems like a lot, but who knows?
+
+            Console.WriteLine("Baking the pizza..."); myPizza.Bake();
+
             Console.WriteLine("Attempting to perform an emergency shut down on the " + myPizza.Kw + " kilowatt Pizza...");
             if (myPizza.EmergencyShutdown() == true) {
                 Console.WriteLine("Shutdown succeeded");
